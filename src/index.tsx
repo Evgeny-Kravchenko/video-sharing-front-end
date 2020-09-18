05b8f6f44b3ab186057512c68c2958c8235786a6
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2020-09-18 16:45:21 +0300

    git: add video cases to the reducer

--- src/index.tsx
+++ src/index.tsx
@@ -13,10 +13,12 @@ import './styles/index.scss';
 
 import store from './store';
 import UserService from './services/user-service';
+import VideoService from './services/video-service';
 
 const AppWithHot = hot(App);
 
 const userService = new UserService();
+const videoService = new VideoService();
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -4,11 +4,16 @@ import IAction from '../interfaces/action.interface';
 import updateCurrentPage from './update-current-page';
 import updateRegisterUser from './update-register-user';
 import updateAuthUser from './update-auth-user';
+import updateUserVideos from './update-user-videos';
 
 const initialState: IState = {
   currentPage: 'auth',
   registerUser: { loading: false, error: null, isSuccess: null, errorMessage: null },
   authUser: { name: '', email: '', isAuth: true, loading: false, error: false },
+  videosOfUser: {
+    ownVideos: { error: null, loading: false, videos: [] },
+    sharedVideos: { error: null, loading: false, videos: [] },
+  },
 };
 
 const reducer = (state: IState = initialState, action: IAction): IState => {
@@ -16,6 +21,7 @@ const reducer = (state: IState = initialState, action: IAction): IState => {
     currentPage: updateCurrentPage(state, action),
     registerUser: updateRegisterUser(state, action),
     authUser: updateAuthUser(state, action),
+    videosOfUser: updateUserVideos(state, action),
   };
 };
 
