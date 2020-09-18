06f4371671b18a0156c65a84499a4a13a0f637bc
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2020-09-18 18:01:08 +0300

    feat: start working on tab in the video page

--- src/components/pages/video/video.page.tsx
+++ src/components/pages/video/video.page.tsx
@@ -1,9 +1,23 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
+
 import VideoList from '../../video-list';
 
 const VideoPage: FC = (): ReactElement => {
+  const [activeVideoPage, setActiveVideoPage] = useState('own')
   return (
-    <div className="p-lg-5 p-md-3 p-2">
+    <div className="py-lg-4 py-md-3 p-2">
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <span className="nav-link">
+            My videos
+          </span>
+        </li>
+        <li className="nav-item">
+          <span className="nav-link active">
+            Video shared to me
+          </span>
+        </li>
+      </ul>
       <VideoList />
     </div>
   );
